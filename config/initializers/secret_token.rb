a7a96ac52af7f4c3da391dbbbff3f92cec5a2987
John Morio Sakaguchi <jsakaguchi@oreilly.com>
2013-08-29 23:06:13 -0700

    Initializing DCS's repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dcs::Application.config.secret_key_base = '7f52491d07efa30342f55546f3a27f93599b7ef98bb6d8ea4595b215eeebd037b8ca83a50ba1b162698646e6c06c84940af8b564e8796ba4339823b1258a11c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dcs::Application.config.session_store :cookie_store, key: '_dcs_session'
